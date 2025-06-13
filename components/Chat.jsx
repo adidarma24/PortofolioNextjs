fdfb8cf24b9918163229c3cab0e1ee3dcf60c114
adidarma24 <adidarmap@gmail.com>
2025-06-13 17:55:45 +0700

    Update Chat Bot

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,13 +1,23 @@
 "use client";
 import { useEffect } from "react";
 
-// Component for Intelliticks chat widget
 const Chat = () => {
   useEffect(() => {
-    // Replace the Intelliticks script here
+    const script = document.createElement("script");
+    script.src = "https://embed.tawk.to/684bf4c87591b5190c90a831/1itkb8cah";
+    script.async = true;
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+
+    document.body.appendChild(script);
+
+    return () => {
+      // Opsional: bersihkan script saat komponen di-unmount
+      document.body.removeChild(script);
+    };
   }, []);
 
-  return null;
+  return null; // Komponen ini tidak merender elemen visual
 };
 
 export default Chat;
